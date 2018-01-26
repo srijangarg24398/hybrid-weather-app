799093cc7448ee2b0b4144895a101135e61a8de0
Srijan <srijan@localhost.localdomain>
2018-01-27 00:25:26 +0530

    for the browser

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather'
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  weather:any;
+  location:{
+  	city:string,
+  	state:string
+  }
+  constructor(public navCtrl: NavController,private weatherProvider:WeatherProvider,private storage:Storage) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  ionViewWillEnter(){
+  	this.storage.get('location').then(val=>{
+  		if (val!=null){
+  			this.location=JSON.parse(val);
+
+  		}else{
+  			this.location={
+		  		city:"Delhi",
+		  		state:"India"
+		  	}
+  		}
+	  	this.weatherProvider.getWeather(this.location.city,this.location.state).subscribe(weather=>{
+	  		this.weather=weather.current_observation
+	  	})
+  	})
   }
 
 }
